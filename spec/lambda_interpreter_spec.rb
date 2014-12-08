f68cd9db4cca8436faebc5fd08935e6745f64f25
Brooklyn Zelenka <brooklyn.zelenka@socialchorus.com>
2014-12-07 22:13:52 -0800

    Added note

--- spec/lambda_interpreter_spec.rb
+++ spec/lambda_interpreter_spec.rb
@@ -1,9 +1,13 @@
 # coding: utf-8
 describe LambdaInterpreter do
- describe 'I/O loop' do
+  # :: String
+  # -> [[char (uniq)], [[sub-expression]], [[sub-expression]]]
+  # -> [sub-expression]
+
+  describe 'I/O loop' do
     describe 'input' do
       it 'reads text from the command line'
-      it 'reads in text once the `enter` key is pressed'
+      it 'reads in text once the `return` key is pressed'
       it 'provides helpful error messages about malformed input'
     end
 
