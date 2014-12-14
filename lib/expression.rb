35764571522101d2df8e9ce9c39bc27ba9e6fa8d
Brooklyn Zelenka <brooklyn.zelenka@socialchorus.com>
2014-12-13 22:09:15 -0800

    Cleaned up `::Expression` somewhat

--- lib/expression.rb
+++ lib/expression.rb
@@ -2,57 +2,46 @@
 # Main class; frames program
 # Reads in string and presents output
 class Expression
-  attr_accessor :ast
+  attr_accessor :parameters,
+                :body,
+                :arguments
 
-  def initialize(string:)
-    @ast = Lex.call(raw: string)
-  end
-
-  def parameters
-    @ast[0]
-  end
-
-  def body
-    @ast[1]
-  end
-
-  def arguments
-    @ast[2]
+  def initialize(raw:)
+    ast = Lex.call(raw: raw)
+    @parameters = ast[0]
+    @body       = ast[1]
+    @arguments  = ast[2]
   end
   
-  def to_s
-    "(#{ parameters_to_s }#{ body_to_s })#{ arguments_to_s }"
+  def add_argument!(raw:)
+    new_argument = Lex.call(raw: raw, taken_variables: variables)
+    @arguments << new_argument
   end
 
   def apply!
-      parameters.each do |param|
-        break unless arguments.first
-        body_indices = body.each_index.select { |i| body[i] == param }
-        body_indices.each { |body_index| body[body_index] = args[index] }
-        arguments.shift
-      end
+    @body.map! { |node| (node == @parameters.first) ? @arguments.first : node }
+
+    @arguments.shift
+    @parameters.shift
   end
-  
+
   def reduce!
-    @ast = Expression::BetaReducer.call(@ast)
+    apply! until parameters.empty? || arguments.empty?
+    body.each { |node| node.reduce! if node.kind_of? Expression }
   end
-
-  private
   
-  def parameters_to_s
-    return if parameters.empty?
-    
-    param_string =  @parameters.reduce { |memo, param| memo.concat param.to_s }
-    "λ{ param_string }."
+  def to_s
+    parameters = "λ{ parameters.join }." unless parameters.empty?
+    body       = body.map(&:to_s).join
+    arguments  = arguments.map { |arg| "(#{ arg })" }.join
+    # Top-level should remove parens
+    "(#{ parameters }#{ body } #{ arguments })"
   end
 
-  def body_to_s
-    return if body.empty?
-    @body.reduce { |memo, node| memo.concat node.to_s }
-  end    
-
-  def arguments_to_s
-    return if arguments.empty?
-    @arguments.reduce { |memo, arg| memo.concat "(#{ arg })" }
+  def variables
+    argument_vars = @arguments.map(&:variables)
+    body_vars = @body.map { |node| node.kind_of?(String) ? node : node.variables }
+    
+    @parameters.concat(argument_vars).concat(body_vars).flatten.uniq
   end
 end
